3eec034f50531cc49bfeeff18e50615406a642c6
khargr14 <kay_hargrove@yahoo.com>
2020-12-29 05:36:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(giftsArray, eventName) {
+    for (let i = 0; i < giftsArray.length; i++){
+      giftsArray.splice(i, 1, `Thank you, ${giftsArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return giftsArray;
+  }
+  
+  function countDown(number) {
+    while(number >= 0){
+      console.log(number);
+      number--;
+    }
+  }
