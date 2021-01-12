53dbe03aa438aab0e5785a3a0e86c21a0a753d87
kam3r0nz <kam.hungerford@gmail.com>
2021-01-11 20:58:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(names, holiday) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]} for the wonderful ${holiday} gift!`)
+    }
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--;
+    }
+}
